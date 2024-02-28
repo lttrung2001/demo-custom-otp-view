1a3bb6059b56b1331f09ab56968355af036d77d0
VPMN\trunglt <trunglt@vnpay.vn>
2024-02-28 16:49:35 +0700

    done

--- app/src/main/java/vn/trunglt/democustomotpview/CustomOtpView.kt
+++ app/src/main/java/vn/trunglt/democustomotpview/CustomOtpView.kt
@@ -3,10 +3,15 @@ package vn.trunglt.democustomotpview
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.CornerPathEffect
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.widget.AppCompatEditText
+
 
 class CustomOtpView(context: Context, attrs: AttributeSet?) :
     AppCompatEditText(context, attrs) {
@@ -25,6 +30,9 @@ class CustomOtpView(context: Context, attrs: AttributeSet?) :
         Paint().apply {
             flags = Paint.ANTI_ALIAS_FLAG
             color = Color.parseColor("#3c3c3c")
+            style = Paint.Style.STROKE
+            strokeWidth = 4f
+//            pathEffect = CornerPathEffect(50f)
         }
     }
     private val letterWrapperRectF by lazy {
@@ -34,14 +42,30 @@ class CustomOtpView(context: Context, attrs: AttributeSet?) :
     private var letterWrapperWidth = 0f
     private var currentX = 0f
 
+    init {
+        setBackgroundDrawable(null)
+        isLongClickable = false
+        isCursorVisible = false
+        movementMethod = null
+        setTextIsSelectable(false)
+    }
+
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        setBackgroundDrawable(null)
+        val desiredWidth = suggestedMinimumWidth + paddingLeft + paddingRight
+        val desiredHeight = suggestedMinimumHeight + paddingTop + paddingBottom
+        setMeasuredDimension(measureDimension(desiredWidth, widthMeasureSpec), measureDimension(desiredHeight, heightMeasureSpec))
     }
 
     override fun onDraw(canvas: Canvas) {
         horizontalDelta = (width / OTP_LENGTH).toFloat()
+//        canvas.drawArc(0f, 0f, 400f, 400f, 180f, 90f, false, letterWrapperPaint);
         drawLetterWrappers(canvas)
+    }
+
+    fun clear() {
+        text?.clear()
+        invalidate()
     }
 
     private fun drawLetterWrappers(canvas: Canvas) {
@@ -49,10 +73,10 @@ class CustomOtpView(context: Context, attrs: AttributeSet?) :
         currentX = 0f
         for (i in 0 until OTP_LENGTH) {
             letterWrapperRectF.set(
-                currentX,
-                0f,
-                currentX + letterWrapperWidth,
-                measuredHeight.toFloat()
+                currentX + paddingStart,
+                paddingTop.toFloat(),
+                currentX + letterWrapperWidth - paddingEnd,
+                measuredHeight.toFloat() - paddingBottom
             )
             canvas.drawRoundRect(letterWrapperRectF, 16f, 16f, letterWrapperPaint)
             if (i < text.toString().length) {
@@ -72,5 +96,23 @@ class CustomOtpView(context: Context, attrs: AttributeSet?) :
             }
             currentX += (LETTER_WRAPPER_PADDING + letterWrapperWidth)
         }
+    }
+
+    private fun measureDimension(desiredSize: Int, measureSpec: Int): Int {
+        var result: Int
+        val specMode = MeasureSpec.getMode(measureSpec)
+        val specSize = MeasureSpec.getSize(measureSpec)
+        if (specMode == MeasureSpec.EXACTLY) {
+            result = specSize
+        } else {
+            result = desiredSize
+            if (specMode == MeasureSpec.AT_MOST) {
+                result = Math.min(result, specSize)
+            }
+        }
+        if (result < desiredSize) {
+            Log.e("ChartView", "The view is too small, the content might get cut")
+        }
+        return result
     }
 }
--- app/src/main/java/vn/trunglt/democustomotpview/MainActivity.kt
+++ app/src/main/java/vn/trunglt/democustomotpview/MainActivity.kt
@@ -2,10 +2,16 @@ package vn.trunglt.democustomotpview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val otpView = findViewById<CustomOtpView>(R.id.customOtpView)
+        val btnClear = findViewById<Button>(R.id.btn_clear)
+        btnClear.setOnClickListener {
+            otpView.clear()
+        }
     }
 }
